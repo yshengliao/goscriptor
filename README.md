e5ad2a25752c9941daaf83aa1c475456605ef5f6
Sheng Liao <sheng@bcowtech.com>
2021-07-16 03:24:49 +0800

    pre-release On branch main
     Initial commit
    
     Changes to be committed:
    	new file:   .gitignore
    	new file:   LICENSE
    	new file:   README.md
    	new file:   doc.go
    	new file:   go.mod
    	new file:   go.sum
    	new file:   redis.go
    	new file:   redisArrayReplyReader.go
    	new file:   redis_test.go
    	new file:   script.go
    	new file:   scriptor.go
    	new file:   scriptor_test.go

---
+++ README.md
@@ -0,0 +1,80 @@
+# goscriptor
+
+Packet GoScriptor implements a way to use redis script more easily
+
+## Install
+
+```console
+go get -u -v github.com/yshengliao/goscriptor
+```
+
+## Usage
+
+Let's start with a trivial example:
+
+```go
+package main
+
+import (
+    "github.com/yshengliao/goscriptor"
+)
+
+var (
+    scriptDefinition = "scriptor_v.0.0.0"
+
+    hello               = "hello"
+    _HelloworldTemplate = `
+    return 'Hello, World!'
+    `
+)
+
+type MyScriptor struct {
+    Scriptor *goscriptor.Scriptor
+}
+
+// hello function
+func (s *MyScriptor) hello() (string, error) {
+    res, err := s.Scriptor.ExecSha(hello, []string{})
+    if err != nil {
+        return "", err
+    }
+
+    return res.(string), nil
+}
+
+func main() {
+    opt := &goscriptor.Option{
+        Host:     "127.0.0.1",
+        Port:     6379,
+        Password: "",
+        DB:       0,
+        PoolSize: 10,
+    }
+
+    scripts := map[string]string{
+        hello: _HelloworldTemplate,
+    }
+
+    scriptor, err := goscriptor.NewDB(opt, 1, scriptDefinition, &scripts)
+    if err != nil {
+        panic(err)
+    }
+
+    myscript := &MyScriptor{
+        Scriptor: scriptor,
+    }
+    res, err := myscript.hello()
+    if err != nil {
+        panic(err)
+    }
+    println(res)
+}
+```
+
+## TODO
+
+1. [ ] Add test cases using "testify".
+2. [ ] Add redis script test method.
+3. [ ] Fix code comments.
+4. [ ] Improve or remove useless code
+5. [ ] Check code formatting
---
+++ scriptor_test.go
@@ -0,0 +1,209 @@
+package goscriptor_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/yshengliao/goscriptor"
+)
+
+const (
+	scriptDefinition    = "scriptKey|0.0.0"
+	hello               = "hello"
+	_HelloworldTemplate = `
+	return 'Hello, World!'
+	`
+)
+
+var (
+	scripts = map[string]string{
+		hello: _HelloworldTemplate,
+	}
+)
+
+func goscriptor_NewDB(host string, port int, scr map[string]string) (*goscriptor.Scriptor, error) {
+	opt := &goscriptor.Option{
+		Host:     host,
+		Port:     port,
+		Password: "",
+		DB:       0,
+		PoolSize: 1,
+	}
+
+	// opt := &goscriptor.Option{
+	// 	Host:     "127.0.0.1",
+	// 	Port:     6379,
+	// 	Password: "",
+	// 	DB:       0,
+	// 	PoolSize: 1,
+	// }
+
+	scriptor, err := goscriptor.NewDB(opt, 1, scriptDefinition, &scr)
+	return scriptor, err
+}
+
+func goscriptor_New(host string, port int, scr map[string]string, assert *assert.Assertions) (*goscriptor.Scriptor, error) {
+	opt := &goscriptor.Option{
+		Host:     host,
+		Port:     port,
+		Password: "",
+		DB:       0,
+		PoolSize: 1,
+	}
+	// opt := &goscriptor.Option{
+	// 	Host:     "127.0.0.1",
+	// 	Port:     6379,
+	// 	Password: "",
+	// 	DB:       0,
+	// 	PoolSize: 1,
+	// }
+
+	redis := opt.Create()
+
+	assert.NotNil(redis)
+
+	scriptor, err := goscriptor.New(redis, 1, scriptDefinition, &scr)
+	return scriptor, err
+}
+
+func goscriptor_TestCase(scriptor *goscriptor.Scriptor, assert *assert.Assertions) {
+	var res interface{}
+	var err error
+
+	res, err = scriptor.Exec("return 'Hello, World!'", []string{""})
+	assert.Nil(err)
+	assert.Equal("Hello, World!", res.(string), "they should be equal")
+
+	res, err = scriptor.Exec("error return 'Hello, World!'", []string{""})
+	assert.NotNil(err)
+
+	res, err = scriptor.ExecSha(hello, []string{""})
+	assert.Nil(err)
+	assert.Equal("Hello, World!", res.(string), "they should be equal")
+
+	res, err = scriptor.ExecSha(hello+" not found", []string{""})
+	assert.NotNil(err)
+	assert.Equal("script not found", err.Error(), "they should be equal")
+}
+
+func Test_goscriptor_NewDB(t *testing.T) {
+	var scriptor *goscriptor.Scriptor
+	var err error
+
+	assert := assert.New(t)
+
+	// Mock Redis
+	s := MockRedisServer()
+	assert.NotNil(s)
+	defer s.Close()
+
+	// scripts does not exist
+	scriptor, err = goscriptor_NewDB(s.Host(), s.Server().Addr().Port, nil)
+	assert.NotNil(err)
+	assert.Equal("Script key does not exist.", err.Error(), "they should be equal")
+
+	// scripts is empty
+	scriptor, err = goscriptor_NewDB(s.Host(), s.Server().Addr().Port, map[string]string{})
+	assert.NotNil(err)
+	assert.Equal("Script key does not exist.", err.Error(), "they should be equal")
+
+	// register scripts
+	scriptor, err = goscriptor_NewDB(s.Host(), s.Server().Addr().Port, scripts)
+	assert.Nil(err)
+	// run test cases
+	goscriptor_TestCase(scriptor, assert)
+
+	// scripts does not exist, and reload redis cache scripts
+	scriptor, err = goscriptor_NewDB(s.Host(), s.Server().Addr().Port, nil)
+	assert.Nil(err)
+	// run test cases
+	goscriptor_TestCase(scriptor, assert)
+
+	// scripts is empty, and reload redis cache scripts
+	scriptor, err = goscriptor_NewDB(s.Host(), s.Server().Addr().Port, map[string]string{})
+	assert.Nil(err)
+	// run test cases
+	goscriptor_TestCase(scriptor, assert)
+
+	// flushAll redis
+	ok, err := scriptor.Client.FlushAll(scriptor.CTX).Result()
+	assert.Nil(err)
+	assert.Equal("OK", ok, "they should be equal")
+
+	// scripts does not exist
+	scriptor, err = goscriptor_NewDB(s.Host(), s.Server().Addr().Port, nil)
+	assert.NotNil(err)
+	assert.Equal("Script key does not exist.", err.Error(), "they should be equal")
+
+	// scripts is empty
+	scriptor, err = goscriptor_NewDB(s.Host(), s.Server().Addr().Port, map[string]string{})
+	assert.NotNil(err)
+	assert.Equal("Script key does not exist.", err.Error(), "they should be equal")
+
+	// can re-register scripts
+	scriptor, err = goscriptor_NewDB(s.Host(), s.Server().Addr().Port, scripts)
+	assert.Nil(err)
+	// run test cases
+	goscriptor_TestCase(scriptor, assert)
+}
+
+func Test_goscriptor_New(t *testing.T) {
+	var scriptor *goscriptor.Scriptor
+	var err error
+
+	assert := assert.New(t)
+
+	// Mock Redis
+	s := MockRedisServer()
+	assert.NotNil(s)
+	defer s.Close()
+
+	// scripts does not exist
+	scriptor, err = goscriptor_New(s.Host(), s.Server().Addr().Port, nil, assert)
+	assert.NotNil(err)
+	assert.Equal("Script key does not exist.", err.Error(), "they should be equal")
+
+	// scripts is empty
+	scriptor, err = goscriptor_New(s.Host(), s.Server().Addr().Port, map[string]string{}, assert)
+	assert.NotNil(err)
+	assert.Equal("Script key does not exist.", err.Error(), "they should be equal")
+
+	// register scripts
+	scriptor, err = goscriptor_New(s.Host(), s.Server().Addr().Port, scripts, assert)
+	assert.Nil(err)
+	// run test cases
+	goscriptor_TestCase(scriptor, assert)
+
+	// scripts does not exist, and reload redis cache scripts
+	scriptor, err = goscriptor_New(s.Host(), s.Server().Addr().Port, nil, assert)
+	assert.Nil(err)
+	// run test cases
+	goscriptor_TestCase(scriptor, assert)
+
+	// scripts is empty, and reload redis cache scripts
+	scriptor, err = goscriptor_New(s.Host(), s.Server().Addr().Port, map[string]string{}, assert)
+	assert.Nil(err)
+	// run test cases
+	goscriptor_TestCase(scriptor, assert)
+
+	// flushAll redis
+	ok, err := scriptor.Client.FlushAll(scriptor.CTX).Result()
+	assert.Nil(err)
+	assert.Equal("OK", ok, "they should be equal")
+
+	// scripts does not exist
+	scriptor, err = goscriptor_NewDB(s.Host(), s.Server().Addr().Port, nil)
+	assert.NotNil(err)
+	assert.Equal("Script key does not exist.", err.Error(), "they should be equal")
+
+	// scripts is empty
+	scriptor, err = goscriptor_NewDB(s.Host(), s.Server().Addr().Port, map[string]string{})
+	assert.NotNil(err)
+	assert.Equal("Script key does not exist.", err.Error(), "they should be equal")
+
+	// can re-register scripts
+	scriptor, err = goscriptor_New(s.Host(), s.Server().Addr().Port, scripts, assert)
+	assert.Nil(err)
+	// run test cases
+	goscriptor_TestCase(scriptor, assert)
+}
